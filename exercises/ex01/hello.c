486d0253a06887684e861c28bf1dd929c9ca12ba
MJ-McMillen <16mmcmillen.stem@gmail.com>
2019-01-31 18:18:56 -0500

    finished optomization section

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+/* Observations:
+  adding the int x = 5 adds code to the assembly language output. (2 lines)
+  adding the int x = 5 does not add extra code to the optomized assembly language output
+  ALSO for next year, please specify that the flag is O2 not 02 I spent 20 min trying to catch that mistake.
 
+  when i print the 5, the code gets shorter.
+
+  My hypothosis is optomisation takes out everything not referenced again. If there is a variable declared but not used the code goes away?
+
+*/
 int main() {
-    printf("Hello, World!\n");
+    int x = 5;
+    int y = x + 1;
+    printf(y);
     return 0;
 }
